7316a4efb718d8570b7ade3e1631c83d9ae039ff
Shaw Kitajima <shawkitajima@gmail.com>
2020-03-03 12:08:23 -0800

    adds functionality to view temperatures upon answer, and move on to next round

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import tempService from './utils/tempService';
 import TempCard from './components/TempCard/TempCard';
 
 function App() {
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
+  const [show, setShow] = useState(false);
+  const [next, setNext] = useState(false);
   // we have to initialize temps because the TempCard props relies on 
   const [temps, setTemps] = useState({
     set1: {},
@@ -15,37 +17,50 @@ function App() {
 
   useEffect(() => {
     tempService.getTemps().then(res => setTemps(res))
-  }, [score])
+  }, [next])
 
   return (
     <div className="App">
       <h1>Welcome to the Temperatue Game!</h1>
       <h2>Which place is currently hotter?</h2>
-      <h1>{score}</h1>
+      <h1>Your score: {score}</h1>
       <div className="flexHori">
         <div className="tempChoice">
           <TempCard choice={temps.set1}/>
-          <button onClick={() => {
+          {show && <h2>{temps.set1.temperature}</h2>}
+          <button disabled={show} onClick={() => {
             if (temps.set1.outcome) {
               setScore(score + 1);
+              setShow(true);
             }
             else {
               setScore(0);
+              setShow(true);
             }
           }}>Me!</button>
         </div>
         <div className="tempChoice">
           <TempCard choice={temps.set2}/>
-          <button onClick={() => {
+          {show && <h2>{temps.set2.temperature}</h2>}
+          <button disabled={show} onClick={() => {
             if (temps.set2.outcome) {
               setScore(score + 1);
+              setShow(true);
             }
             else {
               setScore(0);
+              setShow(true);
             }
           }}>Me!</button>
         </div>
       </div>
+      { show && 
+        <button onClick={() => {
+          setShow(false);
+          setNext(!next);
+        }}>
+          Next
+        </button>}
     </div>
   );
 }
